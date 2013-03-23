2f197e40308485fa6f5a3c38d86a0d8640e6e565
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2013-03-23 19:43:08 +0900

    Generate host_group configuration

--- ec2nagios.php
+++ ec2nagios.php
@@ -21,6 +21,7 @@ if ($match) {
 # List EC2 instances and generate configuration
 $config = '';
 $host_group_members = array();
+
 foreach ($regions as $region) {
 
 	// describe instances in the region.
@@ -55,6 +56,11 @@ foreach ($regions as $region) {
 	}
 
 }
+
+foreach ($host_group_members as $host_group => $members) {
+	$config .= create_host_group_config($host_group, $members);
+}
+
 file_put_contents($ec2nagios_config_path, $config);
 
 function create_host_config($node_dns, $node_name, $node_ip) {
@@ -65,6 +71,23 @@ define host{
         host_name       {$node_dns}
         alias           {$node_name}
         address         {$node_ip}
+        }
+
+EOT;
+
+	return $config;
+
+}
+
+function create_host_group_config($host_group, $members) {
+
+	$members_concat = join($members, ',');
+
+	$config = <<<EOT
+define hostgroup{
+        hostgroup_name  {$host_group}
+        alias           {$host_group}
+        members         {$members_concat}
         }
 
 EOT;
